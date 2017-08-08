87028873c164ce34efc16485d50a1facf9e17126
Blair Englund <blair.englund@gmail.com>
2017-08-08 12:38:06 -0500

    Vue working with rest api

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -4,6 +4,7 @@
     <title><?php wp_title(''); ?></title>
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="https://unpkg.com/tachyons/css/tachyons.min.css">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -1,12 +1,37 @@
 <?php
 get_header();
+?>
 
-get_template_part_pass_vars(
-    'theme/partials/blog-layout',
-    array(
-        'title' => is_front_page() ? get_bloginfo() : get_the_title(get_option('page_for_posts'))
-    )
-);
+<div id="app">
+  <h1>{{ message }}</h1>
+  <button v-on:click="getPost()">Get Data</button>
+  <p>{{ wpData }}</p>
+</div>
 
+<script type="text/javascript">
+	var app = new Vue({
+		el: '#app',
+		data: {
+			message: 'WELCOME TO MY APP',
+			wpData: ''
+		},
+		methods: {
+            getPost() {
+                this.$http.get('wp-json').then(function(response) {
+                	console.log(response.body)
+                    this.$data.wpData = response.body.;
+                }, function(response) {
+                    // Error
+                    console.log(response);
+                });
+            },
+            
+        }
+	})
+
+
+</script>
+
+<?php
 get_footer();
 ?>
