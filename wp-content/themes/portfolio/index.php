4fb3ad01fc27b04611f9bf8f0d752bb3b8f752de
Blair Englund <blair.englund@gmail.com>
2017-09-12 11:57:12 -0500

    updates to styling

--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -2,37 +2,34 @@
 get_header();
 ?>
 <div id="app" class="bg-dark-gray">
-	<article class="vh-100 dt w-100 v-mid avenir cover diamond" v-bind:style="headerStyleObject">
+	<article class="vh-100 dt w-100 v-mid avenir cover diamond">
 		<div class="dtc tc v-mid ph4 relative z-5">
-			<div class="ph2 pv1 br4" v-bind:class="">
-				<h1 class="f-headline fw2 tracked ttl lh1 mb2 white pearl pearl-text">{{ blogTitle }}</h1>
-				<h2 class="f3 fw6 washed-green ttu tracked mb5">{{ blogDescription }}</h2>				
+			<div class="ph2 pv1 br4">
+				<div class="animated fadeInDown">
+					<h1 class="f-headline fw2 tracked ttl lh1 mb2 white pearl pearl-text">{{ blogTitle }}</h1>
+				</div>
+				<h2 class="f3 fw6 washed-green ttu tracked mb5 animated fadeInUp">{{ blogDescription }}</h2>				
 			</div>
 		</div>
 	</article>
 
 	<article id="about" class="vh-100 dt w-100 avenir v-mid pearl">
-		<div class="dtc v-mid black-70 ph3 ph4-l mid-gray">
-			<h3>{{ postData }}</h3>
-			<ul v-if:"typeof postData == Object">
-				<li v-for="(value, index) in postData">
-					<b>{{ index }}</b> - {{ value }}
-				</li>
-			</ul>
-		</div>
+		<div class="dtc v-mid black-70 ph4 pv4 pv6-l ph6-l mid-gray" v-html="postData.content.rendered"></div>
 	</article>
 
-	<article id="projects" class="vh-100 dt w-100 helvetica v-mid">
-		<div class="dark-gray ph3 ph4-l">
-			<div v-for="project in projects">
-				<h3 class="f1">{{ project.title.rendered }}</h3>
+	<article id="projects" class="vh-100 dt w-100 avenir v-mid bg-dark-gray">
+		<div class="ph4 pv4 pv6-l ph6-l">
+			<div class="mb5">
+				<h2 class="f2 tc pearl pearl-text">Recent Work</h2>
 			</div>
-		</div>
-	</article>
-
-	<article id="contact" class="vh-100 dt w-100 helvetica v-mid">
-		<div class="dtc v-mid tc black-70 ph3 ph4-l">
-			
+			<div v-for="project in projects" class="bb pb3 mb5 ">
+				<a v-if="project.acf.link" v-bind:href="project.acf.link" class="db link" target="_blank">
+					<h3 class="f1 mb1 fw2 tracked washed-green hover-washed-red">{{ project.title.rendered }}</h3>
+				</a>
+				<h3 v-else class="f1 mb1 fw2 tracked light-gray">{{ project.title.rendered }}</h3>
+				<h4 class="f4 light-gray" v-html="project.acf.description"></h4>
+				<p class="light-gray f5 lh-copy" v-html="project.acf.responsibilities"></p>
+			</div>
 		</div>
 	</article>
 
@@ -46,9 +43,6 @@ get_header();
 			blogTitle: '<?php echo get_bloginfo('name'); ?>',
 			blogDescription: '<?php echo get_bloginfo('description'); ?>',
 			postData: '',
-			headerStyleObject: {
-				background: '<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0]; ?>' ? ['url(<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0]; ?>)'] : ''
-			},
 			projects: '',
 		},
 		methods: {
@@ -82,7 +76,8 @@ get_header();
         }
 	})
 
-	//app.getProjects()
+	app.getPostData();
+	app.getProjects();
 
 </script>
 
